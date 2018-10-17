0025ed72e3c2add426bc98753f9643659fe28dbe
some-dev <avolkov.dev@gmail.com>
2018-10-17 15:04:27 +0300

    add ability to proceed incoming messages via protobuf

--- contract/Lykke.Job.BlockchainCashoutProcessor.Contract/Commands/StartCashoutCommand.cs
+++ contract/Lykke.Job.BlockchainCashoutProcessor.Contract/Commands/StartCashoutCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using JetBrains.Annotations;
 using MessagePack;
+using ProtoBuf;
 
 namespace Lykke.Job.BlockchainCashoutProcessor.Contract.Commands
 {
@@ -9,24 +10,28 @@ namespace Lykke.Job.BlockchainCashoutProcessor.Contract.Commands
     /// </summary>
     [PublicAPI]
     [MessagePackObject]
+    [ProtoContract]
     public class StartCashoutCommand
     {
         /// <summary>
         /// Cashout operation ID.
         /// </summary>
         [Key(0)]
+        [ProtoMember(0)]
         public Guid OperationId { get; set; }
 
         /// <summary>
         /// Lykke asset ID.
         /// </summary>
         [Key(1)]
+        [ProtoMember(1)]
         public string AssetId { get; set; }
 
         /// <summary>
         /// Recipient address.
         /// </summary>
         [Key(2)]
+        [ProtoMember(2)]
         public string ToAddress { get; set; }
 
         /// <summary>
@@ -34,12 +39,14 @@ namespace Lykke.Job.BlockchainCashoutProcessor.Contract.Commands
         /// Should be positive number.
         /// </summary>
         [Key(3)]
+        [ProtoMember(3)]
         public decimal Amount { get; set; }
 
         /// <summary>
         /// Lykke client ID.
         /// </summary>
         [Key(4)]
+        [ProtoMember(4)]
         public Guid ClientId { get; set; }
     }
 }
