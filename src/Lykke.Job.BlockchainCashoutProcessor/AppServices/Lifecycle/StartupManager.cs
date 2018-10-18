07be6faf9d0a6da6bf6da98dee7711c49adb780d
some-dev <avolkov.dev@gmail.com>
2018-10-18 09:34:40 +0300

    explicity start CQRS Engine in StartupManager

--- src/Lykke.Job.BlockchainCashoutProcessor/AppServices/Lifecycle/StartupManager.cs
+++ src/Lykke.Job.BlockchainCashoutProcessor/AppServices/Lifecycle/StartupManager.cs
@@ -1,20 +1,22 @@
 ﻿using System.Threading.Tasks;
 using JetBrains.Annotations;
+using Lykke.Cqrs;
 
 namespace Lykke.Job.BlockchainCashoutProcessor.AppServices.Lifecycle
 {
-    // NOTE: Sometimes, startup process which is expressed explicitly is not just better, 
-    // but the only way. If this is your case, use this class to manage startup.
-    // For example, sometimes some state should be restored before any periodical handler will be started, 
-    // or any incoming message will be processed and so on.
-    // Do not forget to remove As<IStartable>() and AutoActivate() from DI registartions of services, 
-    // which you want to startup explicitly.
     [UsedImplicitly]
     public class StartupManager : IStartupManager
     {
+        private readonly ICqrsEngine _cqrsEngine;
+
+        public StartupManager(ICqrsEngine cqrsEngine)
+        {
+            _cqrsEngine = cqrsEngine;
+        }
+
         public async Task StartAsync()
         {
-            // TODO: Implement your startup logic here. Good idea is to log every step
+            _cqrsEngine.Start();
 
             await Task.CompletedTask;
         }
